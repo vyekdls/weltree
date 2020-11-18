6d0408b88760fbd4f31a698fe81e7cd272e09917
vyekdls <72438701+vyekdls@users.noreply.github.com>
2020-11-18 10:04:19 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,47 +1,48 @@
-// main.js
-//2단계 메뉴
 $(function(){
-    $('.depth1 > li').hover(
-        function(){
-            $('.depth2').stop().slideDown(200);
-        },
-        function(){
-            $('.depth2').stop().slideUp(200);
-        }
-    )
+    var $firstMenu = $('nav > ul.depth1 > li'),
+        $header = $('#header .container1500'),
+        $logo = $('#header .container1200 > a > .logo')
+        $depth1 = $('#header nav > ul.depth1 > li > a')
+
+        $firstMenu.hover(
+            function(){
+                $header.css({
+                    height: "370px",
+                    background: "url(/img/menu.jpg)"
+                }),
+                $logo.css({
+                    background:"url('/img/header_logo2.png')"
+                }),
+                $depth1.css("color","#000")
+            },
+            function(){
+                $header.css({
+                    height:"84px",
+                    background:"none"
+                }),
+                $logo.css({
+                    background:"url('/img/header_logo1.png')"
+                }),
+                $depth1.css("color","#fff")
+            }
+        )
+
+        $(function(){
+            var header = $('#header'); //헤더를 변수에 넣기
+            var page = $('.page-start'); //색상이 변할 부분
+            var pageOffsetTop = page.offset().top; //색상 변할 부분의 top값 구하기
+            $(window).resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
+              pageOffsetTop = page.offset().top;
+            });
+            
+            $(window).on('scroll', function(){
+              if($(window).scrollTop() >= pageOffsetTop) { // 스크롤이 page보다 밑에 내려가면
+                header.addClass('down'); //클래스 추가
+              } else { // 스크롤 올릴 때
+                header.removeClass('down'); //클래스 제거
+              }
+            });
+          });
+
+
 });
-$(function(){
-    $('.box_1').hover(
-        function(){
-            $('.hover_1').stop().show();
-        },
-        function(){
-            $('.hover_1').stop().hide();
-        }
-    )
-});
-
-$(function(){
-    $('.box_2').hover(
-        function(){
-            $('.hover_2').stop().show();
-        },
-        function(){
-            $('.hover_2').stop().hide();
-        }
-    )
-});
-$(function(){
-    $('.box_3').hover(
-        function(){
-            $('.hover_3').stop().show();
-        },
-        function(){
-            $('.hover_3').stop().hide();
-        }
-    )
-});
-
-
-
-
